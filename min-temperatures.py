26407d36992644f64cd8fb0d8503a2e1a90f2646
Luis Venezian <felipelipe_11@hotmail.com>
2021-01-21 16:24:49 -0300

    min temperature

---
+++ min-temperatures.py
@@ -0,0 +1,23 @@
+from pyspark import SparkConf, SparkContext
+
+conf = SparkConf().setMaster("local").setAppName("MinTemperatures")
+sc = SparkContext(conf = conf)
+
+def parseLine(line):
+    fields = line.split(',')
+    stationID = fields[0] 
+    entryType = fields[2] 
+    # Converting to Fahrenheit
+    temperature = float(fields[3]) * 0.1 * (9.0 / 5.0) + 32.0
+    return (stationID, entryType, temperature)
+
+lines = sc.textFile("file:///Users/luis.vanezian/Documents/spark-learning-handson/1800.csv")
+parsedLines = lines.map(parseLine)
+minTemps = parsedLines.filter(lambda x: "TMIN" in x[1])
+stationTemps = minTemps.map(lambda x: (x[0], x[2])) # Getting stationId and Temperature
+minTemps = stationTemps.reduceByKey(lambda x, y: min(x,y))
+results = minTemps.collect();
+
+print("StationID\tTemperature")
+for result in results:
+    print(result[0] + "\t{:.2f}F".format(result[1]))
