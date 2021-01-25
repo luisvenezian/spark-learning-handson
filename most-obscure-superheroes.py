742990ad2dad185c72b91bb79369c2800b7bedf2
Luis Venezian <felipelipe_11@hotmail.com>
2021-01-25 14:13:12 -0300

    news

--- most-obscure-superheroes.py
+++ most-obscure-superheroes.py
@@ -1,15 +1,8 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Jan 25 11:36:23 2021
-
-@author: luis.vanezian
-"""
-
 from pyspark.sql import SparkSession
 from pyspark.sql import functions as func
 from pyspark.sql.types import StructType, StructField, IntegerType, StringType
 
-spark = SparkSession.builder.appName("LessPopularSuperhero").getOrCreate()
+spark = SparkSession.builder.appName("MostObscureSuperheroes").getOrCreate()
 
 schema = StructType([ \
                      StructField("id", IntegerType(), True), \
@@ -19,12 +12,18 @@ names = spark.read.schema(schema).option("sep", " ").csv("file:///Users/luis.van
 
 lines = spark.read.text("file:///Users/luis.vanezian/Documents/spark-learning-handson/marvel_graph.txt")
 
-# Small tweak vs. what's shown in the video: we trim each line of whitespace as that could
-# throw off the counts.
+# Small tweak vs. what's shown in the video: we trim whitespace from each line as this
+# could throw the counts off by one.
 connections = lines.withColumn("id", func.split(func.trim(func.col("value")), " ")[0]) \
     .withColumn("connections", func.size(func.split(func.trim(func.col("value")), " ")) - 1) \
     .groupBy("id").agg(func.sum("connections").alias("connections"))
     
-connections_filtered = connections.filter(connections.connections == 1)
-connections_filtered.join(names, "id").select("name", "id").show(connections_filtered.count())
+minConnectionCount = connections.agg(func.min("connections")).first()[0]
 
+minConnections = connections.filter(func.col("connections") == minConnectionCount)
+
+minConnectionsWithNames = minConnections.join(names, "id")
+
+print("The following characters have only " + str(minConnectionCount) + " connection(s):")
+
+minConnectionsWithNames.select("name").show()
